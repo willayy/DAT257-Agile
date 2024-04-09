91b5721e6e1f925bc6c673953568b49c21fc708a
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2024-04-09 10:40:41 +0200

    Create show more / show less button in card

--- agile-app/src/components/eventCard.tsx
+++ agile-app/src/components/eventCard.tsx
@@ -1,3 +1,6 @@
+"use client"
+import {useState} from "react";
+
 interface Props {
     data: CardInfo
 }
@@ -16,10 +19,26 @@ interface CardInfo {
 }
 
 export default function EventCard(Props: Props) {
+    const [showMore, setShowMore] = useState<boolean>(false)
+
     return (
         <div>
             <h3>{Props.data.name}</h3>
             <p>{Props.data.summary}</p>
+            {showMore
+                ? (
+                    <>
+                        <p>Type: {Props.data.type}</p>
+                        <p>GPS location: {Props.data.location.gps}</p>
+                        <p>Id: {Props.data.id}</p>
+                        <a href={Props.data.url}>Show event at police website</a>
+                    </>
+                )
+                : <p>Click to show more information</p>
+            }
+            <button onClick={() => setShowMore(!showMore)}>
+                {showMore ? "Show less" : "Show more"}
+            </button>
         </div>
     )
 }
