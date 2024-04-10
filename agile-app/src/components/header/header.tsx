c5e1bbe3ce224e5d2f16b895f050379611b77868
jmarkusson <jomarkusson@gmail.com>
2024-04-10 10:06:01 +0200

    fix links to: händelser and visualisering

--- agile-app/src/components/header/header.tsx
+++ agile-app/src/components/header/header.tsx
@@ -7,13 +7,16 @@ export default function Header() {
       <h1 className={styles.headerTitle}>Brottkollen.se</h1>
       <nav className={styles.nav}>
         <ul className="flex gap-x-6">
+        <li>
+            <Link href="/pages/händelser">Händelser</Link>
+          </li>
           <li>
-            <Link href="/" >Home</Link>
+            <Link href="/pages/visualisering">Visualisering</Link>
           </li>
           <li>
             <Link href="/pages/about">About</Link>
           </li>
-          {/* Add more navigation links here */}
+          
         </ul>
       </nav>
     </header>
