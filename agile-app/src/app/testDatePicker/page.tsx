878e7ac3c0fe39f73292f431b222e0e325e5a1f0
maxdreifeldt <maxdreifeldt02@gmail.com>
2024-04-16 14:06:38 +0200

    DatePicker added and tested.

---
+++ agile-app/src/app/testDatePicker/page.tsx
@@ -0,0 +1,7 @@
+import DatePicker from "@/components/datePicker/datePicker";
+
+export default function TestDatePicker() {
+    return (
+        <DatePicker></DatePicker>
+    )
+}
---
+++ agile-app/src/components/datePicker/datePicker.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React, {ChangeEvent, useState} from 'react'
+const DatePicker = () => {
+
+    const [selectedDate, setSelectedDate] = useState<string>('');
+    const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSelectedDate(event.target.value);
+    };
+
+    return (
+        <div>
+            <label>Select a startdate: </label>
+            <input type="date"
+                   value={selectedDate}
+                   onChange={handleDateChange}
+           />
+            <p>Selected date: {selectedDate}</p>
+
+        </div>
+    );
+
+};
+
+export default DatePicker;
