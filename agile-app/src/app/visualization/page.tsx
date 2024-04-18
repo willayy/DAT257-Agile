b4f1b2530503bcf86ec6141f128cafe5473f9e14
jmarkusson <jomarkusson@gmail.com>
2024-04-18 22:43:48 +0200

    visualization fix

--- agile-app/src/app/visualization/page.tsx
+++ agile-app/src/app/visualization/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import React, { useState } from 'react';
 import ParentSearchComboBox from "@/components/searchComboBox/parentSearchComboBox";
 import styles from './page.module.css';
@@ -6,8 +6,6 @@ import LineGraph from "@/components/lineGraph/lineGraph";
 
 const Visualization = () => {
     const [showGraph, setShowGraph] = useState(false);
-
-    
     const [selectedOptionCrime, setSelectedOptionCrime] = useState<string>('');
     const [selectedOptionLoc, setSelectedOptionLoc] = useState<string>('');
 
@@ -18,11 +16,17 @@ const Visualization = () => {
     return (
         <div className={styles.page}>
             <h1>Visualisering</h1>
-            <ParentSearchComboBox setSelectedOptionCrime={setSelectedOptionCrime} setSelectedOptionLoc={setSelectedOptionLoc}/>
-            <button className={styles.button} onClick={handleClick}>Create Graph</button>
-            <h2>{selectedOptionCrime}</h2>
-            <h2>{selectedOptionLoc}</h2>
-            {showGraph && <LineGraph />}
+            <ParentSearchComboBox
+                setSelectedOptionCrime={setSelectedOptionCrime}
+                setSelectedOptionLoc={setSelectedOptionLoc}
+            />
+            <button className={styles.button} onClick={handleClick}>Create Plot</button>
+            {showGraph && (
+                <LineGraph
+                    selectedOptionCrime={selectedOptionCrime}
+                    selectedOptionLoc={selectedOptionLoc}
+                />
+            )}
         </div> 
     );
 };
