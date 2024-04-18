d7e2c4c04d5670029f3d20883e3c871a1b34a730
William Norland <williamnorland@gmail.com>
2024-04-18 16:07:41 +0200

    added new min max props to components

--- agile-app/src/components/datePicker/datePickerBox.tsx
+++ agile-app/src/components/datePicker/datePickerBox.tsx
@@ -2,6 +2,8 @@ import DatePicker from './datePicker'
 import styles from "./datePicker.module.css";
 
 type DatePickerBoxProps = {
+    max: string;
+    min: string;
     defaultStartDate: string;
     defaultEndDate: string;
     setStartDate: (date: string) => void;
@@ -12,9 +14,8 @@ const DatePickerBox = (props: DatePickerBoxProps) => {
 
     return (
         <div className={styles.dateBoxDiv}>
-            
-            <DatePicker setDate={props.setStartDate} max={props.defaultEndDate} min={props.defaultStartDate} title={"Filtrera datum fr.o.m"} defaultDate={props.defaultStartDate} />
-            <DatePicker setDate={props.setEndDate} max={props.defaultEndDate} min={props.defaultStartDate} title={"t.o.m"} defaultDate={props.defaultEndDate} />
+            <DatePicker setDate={props.setStartDate} max={props.max} min={props.min} title={"Filtrera datum fr.o.m"} defaultDate={props.defaultStartDate} />
+            <DatePicker setDate={props.setEndDate} max={props.max} min={props.min} title={"t.o.m"} defaultDate={props.defaultEndDate} />
         </div>
     )
 }
