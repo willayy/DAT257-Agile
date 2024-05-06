0877076b899e28d48c4b68c3ffb7ebd23cae16c7
William Norland <williamnorland@gmail.com>
2024-05-06 11:06:19 +0200

    basic proj structure

---
+++ agile-app/src/scripts/runFetcher.ts
@@ -0,0 +1,5 @@
+/**
+ * This file contains code for the data fetching sub-program.
+ * While running it continously fetches data from the Polisen API and stores it in .json files
+ * in the data folder.
+ */
