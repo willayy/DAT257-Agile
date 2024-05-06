3dc92644fe03a1857b72f8b0afae325a09c913ab
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2024-05-06 13:45:26 +0200

    Color and style regions on map

--- agile-app/src/app/karta/page.tsx
+++ agile-app/src/app/karta/page.tsx
@@ -2,9 +2,47 @@
 
 import {useEffect, useState} from "react";
 import {fetchRegionData} from "@/scripts/geoFetching";
-import {GeoJSON, MapContainer, TileLayer} from 'react-leaflet'
+import {GeoJSON, GeoJSONProps, MapContainer, TileLayer} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import {GeoJsonObject} from "geojson";
+import {GeoJsonObject, GeoJsonProperties} from "geojson";
+
+type Coordinates = [number, number]; // [longitude, latitude]
+type Polygon = Coordinates[]; // Array of coordinates forming a polygon
+type MultiPolygon = Polygon[]; // Array of polygons forming a multi-polygon
+
+
+interface GeoJsonFeature {
+    "type": 'Feature',
+    "geometry": {
+        "type" : "Polygon"
+        "coordinates": MultiPolygon
+    }
+    "properties": {
+        "name": string,
+        "color": number,
+        "l_id": number
+    }
+}
+
+function getColor(density : number) {
+    return (density > 6 ? '#b30000' :
+            density > 5 ? '#e34a33' :
+            density > 4 ? '#fc8d59' :
+            density > 3 ? '#fdbb84' :
+            density > 2 ? '#fdd49e' :
+            density > 1 ? '#fef0d9' :
+                          '#FFFFFF')
+}
+
+function style() {
+    return {
+        fillColor: getColor(3),
+        weight: 2,
+        opacity: 0.4,
+        color: 'black',
+        fillOpacity: 0.7
+    };
+}
 
 export default function Map() {
     const [regionData, setRegionData] = useState<GeoJsonObject>();
@@ -27,6 +65,7 @@ export default function Map() {
                     <GeoJSON
                         attribution={'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}
                         data={regionData}
+                        style={style}
                     />
                 )}
 
