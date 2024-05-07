d89f0f484ecb463a55199ec7e4f6168003b4eb2b
Erik Andreasson <107652880+0-Gixty-0@users.noreply.github.com>
2024-05-07 11:13:40 +0200

    Fixing bugs

--- agile-app/src/app/karta/page.tsx
+++ agile-app/src/app/karta/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import {useEffect, useState} from "react";
-import {fetchRegionData} from "@/scripts/geoFetching";
+import {fetchMunicipalityData, fetchRegionData} from "@/scripts/geoFetching";
 import {GeoJSON, GeoJSONProps, MapContainer, TileLayer} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import {Feature, GeoJsonObject, GeoJsonProperties, Geometry} from "geojson";
 import ParentSearchComboBox from "@/components/searchComboBox/parentSearchComboBox";
 import styles from "./page.module.css"
+import {getCrimeData} from "@/scripts/dataFetching";
+import {CrimeData} from "@/scripts/dataFetching";
 
 type Coordinates = [number, number]; // [longitude, latitude]
 type Polygon = Coordinates[]; // Array of coordinates forming a polygon
@@ -27,48 +29,85 @@ type MultiPolygon = Polygon[]; // Array of polygons forming a multi-polygon
 // }
 
 interface CustomFeatureProperties {
-    "name": string,
+    "kom_namn": string,
     "color": number,
     "l_id": number
 }
 
+interface NumberDictionary {
+    [key: string]: number;
+}
+
 type CustomFeature = Feature<Geometry, CustomFeatureProperties> | undefined
-
-function getColor(density : number) {
-    return (density > 6 ? '#b30000' :
-            density > 5 ? '#e34a33' :
-            density > 4 ? '#fc8d59' :
-            density > 3 ? '#fdbb84' :
-            density > 2 ? '#fdd49e' :
-            density > 1 ? '#fef0d9' :
-                          '#FFFFFF')
-}
-
-function style(feature: CustomFeature) {
-    if (!feature) {
-        return {}
-    } else {
-        return {
-            fillColor: getColor(feature.properties.color),
-            weight: 2,
-            opacity: 0.4,
-            color: 'black',
-            fillOpacity: 0.7
-        };
-    }
-}
+type Crimes = CrimeData[]
 
 export default function Map() {
-    const [regionData, setRegionData] = useState<GeoJsonObject>();
+    const [regionData, setRegionData] = useState<GeoJsonObject | null>(null);
     const [selectedOptionCrime, setSelectedOptionCrime] = useState<string>('');
     const [selectedOptionLoc, setSelectedOptionLoc] = useState<string>('');
+    const [locationAmountDict, setLocationAmountDict] = useState<NumberDictionary | null>(null)
+
+    async function getEventsOnType(type: string) {
+        const fetchedCrimeData: Crimes = await getCrimeData();
+        console.log(type)
+        console.log(fetchedCrimeData)
+        let locationAmountDict: NumberDictionary = {}
+
+        for (let event of fetchedCrimeData) {
+            if (Object.keys(locationAmountDict).includes(event.location.name) && type == event.type) {
+                locationAmountDict[event.location.name] += 1
+            } else if (type == event.type) {
+                locationAmountDict[event.location.name] = 1
+            }
+        }
+        return locationAmountDict;
+    }
+
+    function getColor(density : number) {
+        return (density > 6 ? '#b30000' :
+            density > 5 ? '#e34a33' :
+                density > 4 ? '#fc8d59' :
+                    density > 3 ? '#fdbb84' :
+                        density > 2 ? '#fdd49e' :
+                            density > 1 ? '#fef0d9' :
+                                '#FFFFFF')
+    }
+
+    function style(feature: CustomFeature) {
+        if (feature == null || locationAmountDict == null) {
+            return {}
+        } else if (Object.keys(locationAmountDict).includes(feature.properties.kom_namn)) {
+            return {
+                fillColor: getColor(locationAmountDict[feature.properties.kom_namn]),
+                weight: 2,
+                opacity: 0.4,
+                color: 'black',
+                fillOpacity: 0.7
+            };
+        } else {
+            return {
+                weight: 3,
+                opacity: 0.6,
+                fillOpacity: 0.2
+            }
+        }
+    }
 
     useEffect(() => {
         const setData = async () => {
-            setRegionData(await fetchRegionData())
+            setRegionData(await fetchMunicipalityData())
         }
         setData()
     })
+
+    useEffect(() => {
+        const setEventsOnType = async () => {
+            setLocationAmountDict(await getEventsOnType(selectedOptionCrime))
+        }
+        setEventsOnType()
+        // console.log(selectedOptionCrime)
+        // console.log(locationAmountDict)
+    }, [selectedOptionCrime])
 
     return (
         <div className={styles.mapWrapper}>
@@ -77,7 +116,7 @@ export default function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {regionData && (
+                {(regionData) && (
                     <GeoJSON
                         attribution={'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}
                         data={regionData}
--- agile-app/src/scripts/geoFetching.ts
+++ agile-app/src/scripts/geoFetching.ts
@@ -15,3 +15,19 @@ export async function fetchRegionData(): Promise<GeoJsonObject> {
 
     return jsonData;
 }
+
+export async function fetchMunicipalityData(): Promise<GeoJsonObject> {
+    const url = new URL("https://raw.githubusercontent.com/okfse/sweden-geojson/master/swedish_municipalities.geojson");
+
+    // Fetch a response from the URL
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data, message: " + res.statusText);
+    }
+
+    // Parse the response to JSON
+    const jsonData = await res.json();
+
+    return jsonData;
+}
