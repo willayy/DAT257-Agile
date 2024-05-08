31008b443de47b61392d3c47830eb8bc99dc5406
William Norland <williamnorland@gmail.com>
2024-05-08 12:37:34 +0200

    prototyping fetching

--- agile-app/src/scripts/runFetcher.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This file contains code for the data fetching sub-program.
- * While running it continously fetches data from the Polisen API and stores it in .json files
- * in the data folder.
- */
