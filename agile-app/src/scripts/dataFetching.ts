7404479bbbf4f0cf27c5186e9e9c605b7a347255
William <williamnorland@gmail.com>
2024-05-10 16:52:34 +0200

    removed unused function

--- agile-app/src/scripts/dataFetching.ts
+++ agile-app/src/scripts/dataFetching.ts
@@ -12,37 +12,6 @@ export interface CrimeData {
     }
 }
 
-// Configs for the data fetch
-const url: URL = new URL("https://polisen.se/api/events");
-const fetchingTimeframe = 60; // This is the timeframe in seconds that the data is cached for
-
-/**
- * Async function that tries to fetch data from the specified URL,
- * if the time since the last call is less than the number of allowed calls per hour
- * the cached data is returned instead.
- * 
- * To use this function in a react component function you need to mark the function as async
- * 
- * @throws Error - If the fetch request fails
- * 
- * @returns crimeDataArray - Array of crime data */
-export async function getLatestFiveHundredCrimeData() {
-
-    // Fetch a response from the URL
-    const res = await fetch(url,{next: {revalidate: fetchingTimeframe}});
-
-    if (!res.ok) {
-        throw new Error("Failed to fetch data, message: " + res.statusText);
-    }
-
-    // Parse the response to JSON and then into a CrimeData array
-    const jsonData = await res.json();
-    const stringData = JSON.stringify(jsonData, null, 2);
-    const crimeDataArray: CrimeData[] = JSON.parse(stringData);
-
-    return crimeDataArray;
-}
-
 /**
  * Function that fetches data from a server side API endpoint.
  * To use this function in a react component function you need to mark the component function as async.
