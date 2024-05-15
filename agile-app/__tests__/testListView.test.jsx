20e651288d766e4770c6011c6364c6db554dda75
William Norland <williamnorland@gmail.com>
2024-05-15 10:46:26 +0200

    mock function

--- agile-app/__tests__/testListView.test.jsx
+++ agile-app/__tests__/testListView.test.jsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import ListView from '../src/components/listView'
- 
+import ListView from '../src/components/listView/listView.tsx'
+
 describe('Page', () => {
   it('renders a heading', () => {
     render(<ListView/>)
