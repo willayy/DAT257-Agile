890a6f4637aa9542cb080fc42c467504e0972026
William Norland <williamnorland@gmail.com>
2024-05-15 10:28:22 +0200

    jest setup

---
+++ agile-app/__tests__/testListView.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import ListView from '../src/components/listView'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<ListView/>)
+ 
+    const heading = screen.getByRole('heading', { level: 1 })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
---
+++ agile-app/__tests__/testSimpleExample.test.ts
@@ -0,0 +1,5 @@
+describe('Test example module', () => {
+  test('Expects 1 to be equal to 1', () => {
+    expect((1)).toBe(1);
+  });
+});
