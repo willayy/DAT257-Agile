5f3f985ed37f994eb0acdb24a9b6a8f9f79dd533
William Norland <williamnorland@gmail.com>
2024-05-17 15:00:34 +0200

    Hotfix test, one line

--- agile-app/__tests__/testLineGraph.test.tsx
+++ agile-app/__tests__/testLineGraph.test.tsx
@@ -8,7 +8,7 @@ describe('LineGraph', () => {
     });
 
     it('renders the chart with correct attributes', () => {
-        const { getByTestId } = render(<LineGraph selectedOptionCrime="" selectedOptionLoc="" />);
+        render(<LineGraph selectedOptionCrime="" selectedOptionLoc="" />);
         const chart = screen.getByTestId('myChart');
         expect(chart).toBeInTheDocument();
         expect(chart).toHaveAttribute('width', '600');
